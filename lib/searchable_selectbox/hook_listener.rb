847abb15efac97d8eb57ac8ebfd1322108814bc7
ishikawa999 <remember929@gmail.com>
2019-12-02 08:20:06 +0000

    Change select2.css to select2.min.css

--- lib/searchable_selectbox/hook_listener.rb
+++ lib/searchable_selectbox/hook_listener.rb
@@ -1,7 +1,7 @@
 module SearchableSelectbox
   class HookListener < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context)
-      stylesheet_link_tag("select2", :plugin => "redmine_searchable_selectbox") +
+      stylesheet_link_tag("select2.min", :plugin => "redmine_searchable_selectbox") +
       stylesheet_link_tag("searchable_selectbox", :plugin => "redmine_searchable_selectbox") +
       javascript_include_tag("select2.full.min.js", :plugin => "redmine_searchable_selectbox") +
       javascript_include_tag("searchable_selectbox.js", :plugin => "redmine_searchable_selectbox")
