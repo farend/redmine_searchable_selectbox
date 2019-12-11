2ad784706588e1f229028a65479b59158ed8ac91
ishikawa999 <remember929@gmail.com>
2019-12-11 05:40:52 +0000

    Change version

--- init.rb
+++ init.rb
@@ -4,5 +4,5 @@ require 'searchable_selectbox/hook_listener'
 Redmine::Plugin.register :redmine_searchable_selectbox do
   name 'Redmine Searchable Selectbox'
   description "This plugin changes Redmine's selectbox searchable."
-  version '0.0.1'
+  version '0.1.0'
 end
