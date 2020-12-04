0f577a1c79bebf2d4f6f70fc0d4ab7e5869ca519
ishikawa999 <remember929@gmail.com>
2020-12-04 01:46:00 +0000

    Fix My page selectbox

--- init.rb
+++ init.rb
@@ -1,10 +1,17 @@
 require 'redmine'
 require 'searchable_selectbox/hook_listener'
+require 'searchable_selectbox/patches/my_helper_patch'
 
 Redmine::Plugin.register :redmine_searchable_selectbox do
   name 'Redmine Searchable Selectbox'
   description "This plugin changes Redmine's selectbox searchable."
   version '0.1.0'
 
-  settings default: {'enabled' => 1}, :partial => 'settings/searchable_selectbox_settings'
+  settings default: {'enabled' => 1}, partial: 'settings/searchable_selectbox_settings'
+end
+
+Rails.configuration.to_prepare do
+  unless MyHelper.included_modules.include?(MyHelperPatch)
+    MyHelper.prepend(MyHelperPatch)
+  end
 end
