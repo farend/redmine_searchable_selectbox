f35ae2c5eb160ba45e9131ea973788859938face
ishikawa <ishikawa@farend.jp>
2022-02-18 05:01:40 +0000

    Change to trigger the change event twice only when there is a native change event (redmica/redmica_ui_extension 3c95e9a)

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ require 'searchable_selectbox/patches/my_helper_patch'
 Redmine::Plugin.register :redmine_searchable_selectbox do
   name 'Redmine Searchable Selectbox'
   description "This plugin changes Redmine's selectbox searchable."
-  version '0.1.5'
+  version '0.1.7'
 
   settings default: {'enabled' => 1}, partial: 'settings/searchable_selectbox_settings'
 end
