79965fd2fbcedfd7913721dd89aaee67fa2f8ae8
ishikawa999 <remember929@gmail.com>
2022-05-20 01:48:34 +0000

    Fix uninitialized constant SearchableSelectbox::Patches::MyHelperPatch

--- lib/searchable_selectbox/patches/my_helper_patch.rb
+++ lib/searchable_selectbox/patches/my_helper_patch.rb
@@ -1,16 +1,20 @@
 require_dependency 'my_helper'
 
-module MyHelperPatch
-  def self.prepended(base)
-    base.send(:prepend, InstanceMethods)
-  end
+module SearchableSelectbox
+  module Patches
+    module MyHelperPatch
+      def self.prepended(base)
+        base.send(:prepend, InstanceMethods)
+      end
 
-  module InstanceMethods
-    # Run replaceSelect2(); when remove_block.js.erb
-    def block_select_tag(user)
-      super(user) +
-      javascript_tag do
-        '$(function(){replaceSelect2();});'
+      module InstanceMethods
+        # Run replaceSelect2(); when remove_block.js.erb
+        def block_select_tag(user)
+          super(user) +
+          javascript_tag do
+            '$(function(){replaceSelect2();});'
+          end
+        end
       end
     end
   end
